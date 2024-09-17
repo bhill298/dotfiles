d4ca0f33e1055b9fb34357d70af3206628061888
bhill <bhill298@users.noreply.github.com>
2024-09-16 18:25:21 -0600

    Create Microsoft.PowerShell_profile.ps1

---
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+# can get through $profile (default %USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1)
+Set-PSReadLineKeyHandler -Chord Ctrl-a -Function BeginningOfLine
+Set-PSReadLineKeyHandler -Chord Ctrl-e -Function EndOfLine
+Set-PSReadLineKeyHandler -Chord Ctrl+u -Function BackwardDeleteLine
+Set-PSReadLineKeyHandler -Chord Ctrl+k -Function ForwardDeleteLine                                                                        

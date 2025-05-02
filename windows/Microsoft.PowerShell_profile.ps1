79642292fc92b3323fdae7a47a8b4048702ae94d
bhill <bhill298@users.noreply.github.com>
2025-05-02 11:58:16 -0600

    Update Microsoft.PowerShell_profile.ps1

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -4,5 +4,5 @@ Set-PSReadLineKeyHandler -Chord Ctrl-e -Function EndOfLine
 Set-PSReadLineKeyHandler -Chord Ctrl+u -Function BackwardDeleteLine
 Set-PSReadLineKeyHandler -Chord Ctrl+k -Function ForwardDeleteLine                                                                        
 
-function _disable_history { Set-PSReadlineOption -HistorySaveStyle SaveNothing }
-New-Alias dh _disable_history
+function dh { Set-PSReadlineOption -HistorySaveStyle SaveNothing }
+function StopSound { (New-Object Media.SoundPlayer).Stop() }

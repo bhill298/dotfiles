dd3bba722703ef08e7f9a0a776098ee8de5b9d22
bhill <bhill298@users.noreply.github.com>
2025-05-02 10:59:55 -0600

    Update Microsoft.PowerShell_profile.ps1

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,6 @@ Set-PSReadLineKeyHandler -Chord Ctrl-a -Function BeginningOfLine
 Set-PSReadLineKeyHandler -Chord Ctrl-e -Function EndOfLine
 Set-PSReadLineKeyHandler -Chord Ctrl+u -Function BackwardDeleteLine
 Set-PSReadLineKeyHandler -Chord Ctrl+k -Function ForwardDeleteLine                                                                        
+
+function _disable_history { Set-PSReadlineOption -HistorySaveStyle SaveNothing }
+New-Alias dh _disable_history

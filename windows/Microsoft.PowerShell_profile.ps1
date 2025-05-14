e1ab82891f9917b842b395bd6790941fa7c9ee49
bhill <bhill298@users.noreply.github.com>
2025-05-13 20:10:35 -0600

    Update Microsoft.PowerShell_profile.ps1

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,29 @@
 Set-PSReadLineKeyHandler -Chord Ctrl-a -Function BeginningOfLine
 Set-PSReadLineKeyHandler -Chord Ctrl-e -Function EndOfLine
 Set-PSReadLineKeyHandler -Chord Ctrl+u -Function BackwardDeleteLine
-Set-PSReadLineKeyHandler -Chord Ctrl+k -Function ForwardDeleteLine                                                                        
+Set-PSReadLineKeyHandler -Chord Ctrl+k -Function ForwardDeleteLine
 
+# source: https://github.com/majkinetor/posh/blob/master/MM_Admin/Invoke-Environment.ps1
+function Invoke-Environment {
+    param
+    (
+        # Any cmd shell command, normally a configuration batch file.
+        [Parameter(Mandatory=$true)]
+        [string] $Command
+    )
+
+    $Command = "`"" + $Command + "`""
+    cmd /c "$Command > nul 2>&1 && set" | . { process {
+        if ($_ -match '^([^=]+)=(.*)') {
+            [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2])
+        }
+    }}
+
+}
+
+# vcvars32.bat vcvars64.bat needs to be on path
+# e.g. C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\
+function vcvars { Invoke-Environment $(which vcvars64.bat) }
+function vcvars32 { Invoke-Environment $(which vcvars32.bat) }
 function dh { Set-PSReadlineOption -HistorySaveStyle SaveNothing }
 function StopSound { (New-Object Media.SoundPlayer).Stop() }

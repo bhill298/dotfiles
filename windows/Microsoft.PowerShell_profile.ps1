f9d3a9be59bb15939537ae32c7d0b6739b5f912e
bhill <bhill298@users.noreply.github.com>
2026-05-15 08:30:22 -0600

    Update powershell vcvars

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -14,19 +14,17 @@ function Invoke-Environment {
         [Parameter(Mandatory=$true)]
         [string] $Command
     )
-
     $Command = "`"" + $Command + "`""
     cmd /c "$Command > nul 2>&1 && set" | . { process {
         if ($_ -match '^([^=]+)=(.*)') {
             [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2])
         }
     }}
-
 }
 
 # vcvars32.bat vcvars64.bat needs to be on path
 # e.g. C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\
-function vcvars { Invoke-Environment  $((get-command vcvars64.bat).Path) }
+function vcvars64 { Invoke-Environment  $((get-command vcvars64.bat).Path) }
 function vcvars32 { Invoke-Environment  $((get-command vcvars32.bat).Path) }
 function dh { Set-PSReadlineOption -HistorySaveStyle SaveNothing }
 function StopSound { (New-Object Media.SoundPlayer).Stop() }
